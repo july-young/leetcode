da6e42aee5c2b2cde824e2037c4740d516dbf506
july_young <1103225671@qq.com>
2023-03-02 18:42:29 +0800

    feat: add 0014 0015

---
+++ src/main/java/com/july/leetcode/ten/Fourteen.java
@@ -0,0 +1,23 @@
+package com.july.leetcode.ten;
+
+public class Fourteen {
+    public static void main(String[] args) {
+        System.out.println(new Fourteen().longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
